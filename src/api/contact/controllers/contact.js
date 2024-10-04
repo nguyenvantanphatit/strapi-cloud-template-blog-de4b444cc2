5e7feba749feb78160ddae5490db47e1df299c62
Phat <nvtpk19hutech@gmail.com>
2024-10-04 11:30:30 +0700

    12

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -10,7 +10,7 @@ module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
   async findOne(ctx) {
     const { slug } = ctx.params;
 
-    const entity = await strapi.db.query('api::contact.contact').findOne({
+    const entity = await strapi.query('api::contact.contact').findOne({
       where: { slug },
       populate: { deep: 4 },
     });
@@ -36,7 +36,7 @@ module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
     try {
       const { name, email, phone, message, status_contact } = ctx.request.body;
 
-      const newContact = await strapi.db.query('api::contact.contact').create({
+      const newContact = await strapi.query('api::contact.contact').create({
         data: {
           name,
           email,
